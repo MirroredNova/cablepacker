218605f35b47f15d44e84d2016258c28726bd805
Nate Wiltzius <natewiltzius@alliantenergy.com>
2024-12-27 18:36:43 -0600

    Added MUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,16 @@
 import React from 'react';
+import { Roboto } from 'next/font/google';
+import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import { CssBaseline } from '@mui/material';
+import theme from '../theme';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
+});
 
 type Props = Readonly<{
   children: React.ReactNode;
@@ -7,7 +19,16 @@ type Props = Readonly<{
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={roboto.variable}>
+        <StyledEngineProvider injectFirst>
+          <AppRouterCacheProvider>
+            <ThemeProvider theme={theme}>
+              {children}
+              <CssBaseline />
+            </ThemeProvider>
+          </AppRouterCacheProvider>
+        </StyledEngineProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
+'use client';
+
 import React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
 
 export default function Home() {
-  return <div>template</div>;
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+      <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+        <Tab label="Item One" />
+        <Tab label="Item Two" />
+        <Tab label="Item Three" />
+      </Tabs>
+    </Box>
+  );
 }
