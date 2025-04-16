6c34fd09b86e0bcb71410ff47d88fd5c5809762a
Nate Wiltzius <natewiltzius@alliantenergy.com>
2025-04-16 17:17:29 -0500

    Fixing deployment tests

--- terraform/web-app.tf
+++ terraform/web-app.tf
@@ -18,22 +18,7 @@ resource "azurerm_linux_web_app" "app" {
   }
   
   app_settings = var.app_service_settings
-
-  logs {
-    detailed_error_messages = var.app_service_logs_detailed_error_messages
-    failed_request_tracing  = var.app_service_logs_failed_request_tracing
-    http_logs {
-      file_system {
-        retention_in_days = var.app_service_logs_file_system_retention_in_days
-        retention_in_mb   = var.app_service_logs_file_system_retention_in_mb
-      }
-    }
-  }
   tags = azurerm_resource_group.rg.tags
-  // virtual_network_subnet_id resets to NULL when connecting to virtual network using azurerm_app_service_virtual_network_swift_connection
-  // Which in-turn removes the app-out subnet connection. Provided fix is to ignore virtual_network_subnet_id value as both methods can't be used simultaneously
-  // GitHub issue link: https://github.com/hashicorp/terraform-provider-azurerm/issues/17930#issuecomment-1216719591
-  lifecycle { ignore_changes = [virtual_network_subnet_id] }
 }
 
 output "web_app_service_id" {
