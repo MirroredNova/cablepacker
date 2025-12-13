4eb502b4327e073b415bcbc21eeb13b3592c8f23
Nate Wiltzius <novawiltzius@gmail.com>
2025-12-13 09:38:14 +0900

    Added container to results table

--- src/components/pages/home/results/ResultsCables.tsx
+++ src/components/pages/home/results/ResultsCables.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Table from '@mui/material/Table';
+import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
@@ -37,41 +38,43 @@ function ResultsCables({ cables }: Props) {
   return (
     <Box display="flex" flexDirection="column" gap="8px" flex={1} className="ResultsCables">
       <Typography variant="h5">Cables</Typography>
-      <Table size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="right">Diameter (in)</TableCell>
-            <TableCell align="right">Quantity</TableCell>
-            <TableCell align="center">Color</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {uniqueCables.map((cable) => (
-            <TableRow
-              key={`${cable.name}-${cable.diameter}`}
-              sx={{ '&:nth-of-type(odd)': { backgroundColor: 'rgba(0, 0, 0, 0.04)' } }}
-            >
-              <TableCell>{cable.name}</TableCell>
-              <TableCell align="right">{cable.diameter.toFixed(3)}</TableCell>
-              <TableCell align="right">{cable.quantity}</TableCell>
-              <TableCell align="center">
-                <Box
-                  sx={{
-                    width: 24,
-                    height: 24,
-                    backgroundColor: cable.color,
-                    margin: '0 auto',
-                    borderRadius: '50%',
-                    border: '1px solid rgba(0, 0, 0, 0.2)',
-                  }}
-                  title={`${cable.name} (${cable.diameter}in)`}
-                />
-              </TableCell>
+      <TableContainer>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell align="right">Diameter (in)</TableCell>
+              <TableCell align="right">Quantity</TableCell>
+              <TableCell align="center">Color</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHead>
+          <TableBody>
+            {uniqueCables.map((cable) => (
+              <TableRow
+                key={`${cable.name}-${cable.diameter}`}
+                sx={{ '&:nth-of-type(odd)': { backgroundColor: 'rgba(0, 0, 0, 0.04)' } }}
+              >
+                <TableCell>{cable.name}</TableCell>
+                <TableCell align="right">{cable.diameter.toFixed(3)}</TableCell>
+                <TableCell align="right">{cable.quantity}</TableCell>
+                <TableCell align="center">
+                  <Box
+                    sx={{
+                      width: 24,
+                      height: 24,
+                      backgroundColor: cable.color,
+                      margin: '0 auto',
+                      borderRadius: '50%',
+                      border: '1px solid rgba(0, 0, 0, 0.2)',
+                    }}
+                    title={`${cable.name} (${cable.diameter}in)`}
+                  />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
   );
 }
